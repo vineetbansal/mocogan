f8b3dd88c2dd9ea01851c413b919fa014eb33574
Vineet Bansal <vineetbansal@protonmail.com>
2023-04-28 17:39:14 -0400

    works!

--- src/logger.py
+++ src/logger.py
@@ -15,34 +15,18 @@ except ImportError:
 
 class Logger(object):
     def __init__(self, log_dir, suffix=None):
-        self.writer = tf.summary.FileWriter(log_dir, filename_suffix=suffix)
+        #self.writer = tf.summary.FileWriter(log_dir, filename_suffix=suffix)
+        self.writer = tf.summary.create_file_writer(log_dir)
 
     def scalar_summary(self, tag, value, step):
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value.item(), step)
+        #summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        #self.writer.add_summary(summary, step)
 
     def image_summary(self, tag, images, step):
-
-        img_summaries = []
-        for i, img in enumerate(images):
-            # Write the image to a string
-            try:
-                s = StringIO()
-            except:
-                s = BytesIO()
-            scipy.misc.toimage(img).save(s, format="png")
-
-            # Create an Image object
-            img_sum = tf.Summary.Image(encoded_image_string=s.getvalue(),
-                                       height=img.shape[0],
-                                       width=img.shape[1])
-            # Create a Summary value
-            img_summaries.append(tf.Summary.Value(tag='%s/%d' % (tag, i), image=img_sum))
-
-        # Create and write Summary
-        summary = tf.Summary(value=img_summaries)
-        self.writer.add_summary(summary, step)
-        self.writer.flush()
+        with self.writer.as_default():
+            tf.summary.image(name='%s_images' % (tag,), data=images, step=step)
 
     def video_summary(self, tag, videos, step):
 
@@ -52,28 +36,14 @@ class Logger(object):
         separator = np.zeros(sh, dtype=videos.dtype)
         videos = np.concatenate([videos, separator], axis=-1)
 
-        img_summaries = []
-        for i, vid in enumerate(videos):
-            # Concat a video
-            try:
-                s = StringIO()
-            except:
-                s = BytesIO()
+        with self.writer.as_default():
+            for i, vid in enumerate(videos):
 
-            v = vid.transpose(1, 2, 3, 0)
-            v = [np.squeeze(f) for f in np.split(v, v.shape[0], axis=0)]
-            img = np.concatenate(v, axis=1)[:, :-1, :]
+                v = vid.transpose(1, 2, 3, 0)
+                v = [np.squeeze(f) for f in np.split(v, v.shape[0], axis=0)]
 
-            scipy.misc.toimage(img).save(s, format="png")
+                # create one looong image with everything stacked horizontally
+                # img = np.concatenate(v, axis=1)[:, :-1, :]
+                img = np.stack(v)
 
-            # Create an Image object
-            img_sum = tf.Summary.Image(encoded_image_string=s.getvalue(),
-                                       height=img.shape[0],
-                                       width=img.shape[1])
-            # Create a Summary value
-            img_summaries.append(tf.Summary.Value(tag='%s/%d' % (tag, i), image=img_sum))
-
-        # Create and write Summary
-        summary = tf.Summary(value=img_summaries)
-        self.writer.add_summary(summary, step)
-        self.writer.flush()
+                tf.summary.image(name='%s/%d' % (tag, i), data=img, step=step)
